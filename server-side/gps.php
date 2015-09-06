2f0117f959124e3f5f1a26613d2b33eb5b4214b2
Hervé Renault <hr@herverenault.fr>
2015-09-06 21:19:04 +0200

    Fix previous commit... and add comments into the demo script.

--- server-side/gps.php
+++ server-side/gps.php
@@ -1,13 +1,16 @@
 <?php
 
-$file = "/tmp/gps-position.txt";
+$file = "/tmp/gps-position.txt"; // you might save in a database instead...
 
-if (isset($_GET["tracker"])
-    || isset($_GET["lat"]) && preg_match("/^-?\d+.\d+$/", $_GET["lat"])
-    && isset($_GET["lon"]) && preg_match("/^-?\d+.\d+$/", $_GET["lon"]) ) {
+if (isset($_GET["lat"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lat"])
+    && isset($_GET["lon"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lon"]) ) {
 	$f = fopen($file,"w");
 	fwrite($f, date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]);
 	fclose($f);
+    // you should obviously do your own checks before this...
+    echo "OK";
+} elseif (isset($_GET["tracker"])) {
+    // do whatever you want here...
     echo "OK";
 } else {
     header('HTTP/1.0 400 Bad Request');
