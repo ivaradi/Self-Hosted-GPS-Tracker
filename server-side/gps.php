62eb3efba0c3d056144ab5d9f4c283089f9edd9b
Hervé Renault <hr@herverenault.fr>
2015-09-06 19:30:16 +0200

    Allow tracker=start/stop requests.

--- server-side/gps.php
+++ server-side/gps.php
@@ -2,7 +2,8 @@
 
 $file = "/tmp/gps-position.txt";
 
-if ( isset($_GET["lat"]) && preg_match("/^-?\d+.\d+$/", $_GET["lat"])
+if (isset($_GET["tracker"])
+    || isset($_GET["lat"]) && preg_match("/^-?\d+.\d+$/", $_GET["lat"])
     && isset($_GET["lon"]) && preg_match("/^-?\d+.\d+$/", $_GET["lon"]) ) {
 	$f = fopen($file,"w");
 	fwrite($f, date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]);
