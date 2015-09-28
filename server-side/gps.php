ddc493667ad25be248fd5e76f965a8fd5cd17123
Hervé Renault <hr@herverenault.fr>
2015-09-28 12:28:25 +0200

    Add error handling when writing to file.
    This is only a sample script, but many use it as is and then email me only
    to find out that they can't write to file for some reason.  This should
    help them sort it out by themselves. (Although I like chatting with
    users ;)

--- server-side/gps.php
+++ server-side/gps.php
@@ -1,15 +1,22 @@
 <?php
 
+// THIS IS ONLY A SAMPLE SCRIPT. PLEASE TWEAK IT TO YOUR NEEDS.
+// IT COMES WITH NO WARRANTY WHATSOEVER. 
+
 $file = "/tmp/gps-position.txt"; // you might save in a database instead...
 
 if (isset($_GET["lat"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lat"])
     && isset($_GET["lon"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lon"]) ) {
-    $f = fopen($file,"w");
-    fwrite($f, date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]."_");
-    if (isset($_GET["t"]) && preg_match("/^\d+$/", $_GET["t"])) {
-        fwrite($f, $_GET["t"]);
+    $fh = fopen($file, "w");
+    if (!$fh) {
+        header("HTTP/1.0 500 Internal Server Error");
+        exit(print_r(error_get_last(), true));
     }
-    fclose($f);
+    fwrite($fh, date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]."_");
+    if (isset($_GET["t"]) && preg_match("/^\d+$/", $_GET["t"])) {
+        fwrite($fh, $_GET["t"]);
+    }
+    fclose($fh);
     // you should obviously do your own checks before this...
     echo "OK";
 } elseif (isset($_GET["tracker"])) {
